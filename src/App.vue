78bebe8d21037476b07ef7db479e0aae576b437b
Arbi Kullakshi <arbikullakshi0@gmail.com>
2025-08-08 10:49:47 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import ServiceFrequency from "@/components/ServiceFrequency.vue";
+
+export default defineComponent({
+  components: {ServiceFrequency}
+})
+</script>
+
+<template>
+  <ServiceFrequency/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ src/components/ServiceFrequency.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-9xl">Hi</h1>
+</template>
+
